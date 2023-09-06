debb21c75e11b48592559b99a897387f83a4c665
batishavo <romero.martinez.carlosraul@gmail.com>
2023-09-06 15:35:41 -0600

    avance en el consumo de la api

--- javascript/consumir-api.js
+++ javascript/consumir-api.js
@@ -1,3 +1,5 @@
+import {comicToJson,toComic} from "./comic.js";
+
 const getComics = (done)=>{
     const conexion_string = "https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&noVariants=true&dateDescriptor=thisWeek&apikey=3b03881e336f538f010165c962d08abe&hash=45f10393a9682d517474ac2ad2c28622&ts=1000";
     const result = fetch(conexion_string);
@@ -12,14 +14,28 @@ const getComics = (done)=>{
 getComics(data=>{
     
     //console.log(data);
+    //console.log(data.data.results);
+    //console.log(toComic(data));
+    //console.log();
+    
+    let algo = comicToJson(data);
+    let comics = toComic(algo);
 
-    data.result.array.forEach(comic => {
-        console.log(comic.thumbnail.path);
-        const article = document.createRange().createContextualFragment(/*html*/`
+    //console.log(comics);
+
+    comics.data.results.forEach(comic => {
+        //console.log(comic)   
+        // console.log(comic.images[0].path);
+        // console.log(comic.images[0].extension)
+        //
+
+        const article = document.createRange().createContextualFragment(
+            /*html*/
+            `
             <div class="card">
                 <div class="card-image">
                     <figure class="image is-4by3">
-                        <img src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="${comic.title}">
+                        <img src="${comic.images[0].path}.${comic.images[0].extension}" alt="${comic.title}">
                     </figure>
                 </div>
                 <div class="card-content">
@@ -30,9 +46,11 @@ getComics(data=>{
                     </div>
                 </div>
             </div>
-        `)
-        const main = document.querySelector("comics");
-        main.append(article)
+        `
+        )
+        const main = document.querySelector("main");
+        main.append(article);
+        
     });
 
 });
