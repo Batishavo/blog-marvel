10b17b2aacd9a7480c3cdf068f788ffb8b0e6ca1
batishavo <romero.martinez.carlosraul@gmail.com>
2023-09-11 15:16:02 -0600

    avance

--- javascript/consumir-api.js
+++ javascript/consumir-api.js
@@ -15,15 +15,15 @@ getComics(data=>{
     
     //console.log(data);
     //console.log(data.data.results);
+    //console.log(comicToJson(data));
     //console.log(toComic(data));
-    //console.log();
     
-    let algo = comicToJson(data);
-    let comics = toComic(algo);
+    //let algo = comicToJson(data);
+    //let comics = toComic(algo);
 
     //console.log(comics);
 
-    comics.data.results.forEach(comic => {
+    data.data.results.forEach(comic => {
         //console.log(comic)   
         // console.log(comic.images[0].path);
         // console.log(comic.images[0].extension)
@@ -32,23 +32,29 @@ getComics(data=>{
         const article = document.createRange().createContextualFragment(
             /*html*/
             `
-            <div class="card">
-                <div class="card-image">
-                    <figure class="image is-4by3">
-                        <img src="${comic.images[0].path}.${comic.images[0].extension}" alt="${comic.title}">
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-content">
-                            <p class="title is-4">${comic.title}</p>
+                <div class="card col-xl-3 col-lg-5 col-md-7 col-sm-9 col-12 mb-5 m">
+                    <div class="card-header">
+                        <h3 class="title is-4">${comic.title}</h3>
+                    </div>
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img class="img-fluid" src="${comic.images[0].path}.${comic.images[0].extension}"
+                                alt="${comic.title}">
+                        </figure>
+                    </div>
+                    <div class="card-body">
+                        <div class="media">
+                            <div class="media-content">
+                                <h4>${comic.dates.date}</h4>
+                                <p>${comic.series.name}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+
         `
         )
-        const main = document.querySelector("main");
+        const main = document.querySelector("#comics");
         main.append(article);
         
     });
